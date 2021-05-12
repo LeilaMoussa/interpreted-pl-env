6aa84c6a8f1114a92289f69ecb34cf4df685ebe6
LeilaMoussa <58636465+LeilaMoussa@users.noreply.github.com>
2021-05-12 16:13:27 +0000

    pushing because computer may crash

--- milestone4/cst.py
+++ milestone4/cst.py
@@ -1,34 +1,80 @@
+from os import stat
+
+
 class ParseTreeNode():
     # If any functions are general to all tree nodes,
     # they should be put here
     # maybe a display() function will come in handy
     # but we'll need one for each subclass as well
+    # if there are no shared functions aamong these, no need for inheritance
     pass
 
-class ProgramNode(ParseTreeNode):
-    def __init__(self, declarations: list, functions: list, mainFunction: MainNode):
-        self.declarations = declarations
-        self.functions = functions
-        self.mainFunction = mainFunction
-    def append_declaration(self, dec: DeclarationNode):
-        self.declarations.append(dec)
-    def append_function(self, func: FunctionNode):
-        self.functions.append(func)
-
-class MainNode(ParseTreeNode):  # this means inheritance
-    def __init__(self, declarations: list, statements: list):
-        # we won't implement nested functions
-        self.declarations = declarations  # of type DeclarationNode
-        self.statements = statements  # of type StatementNode
-    def append_declaration(self, dec: DeclarationNode):
-        self.declarations.append(dec)
-    def append_statement(self, stat: StatementNode):
-        self.statements.append(stat)
+class TypeNode(ParseTreeNode):
+    def __init__(self, value: str):
+        self.value = value  # num, ascii
 
 class VarDeclarationNode(ParseTreeNode):
     def __init__(self, typespec: TypeNode, identifier: str):
         self.typespec = typespec
         self.identifier = identifier
+
+class OperationNode():
+    def __init__(self):
+        pass
+
+class UserDefinedNode(ParseTreeNode):
+    def __init__(self, name: str):
+        self.name = name
+
+class ReservedNode(ParseTreeNode):
+    def __init__(self, value: str):
+        self.value = value  # write or read
+
+class IdentifierNode(ParseTreeNode):
+    def __init__(self, udi: UserDefinedNode, res: ReservedNode):
+        if udi:
+            self.type = 'userdefined'
+            self.value = udi
+        elif res:
+            self.type = 'reserved'
+            self.value = res
+        else:
+            raise Exception("nothing matches in id node")
+
+class CallNode(ParseTreeNode):  # function call
+    def __init__(self, name: IdentifierNode, args: list):
+        self.name = name
+        self.args = args  # list[ExpressionNode]
+
+class StringLiteralNode(ParseTreeNode):
+    def __init__(self, value: str):
+        self.value = value
+
+class NumLiteralNode(ParseTreeNode):
+    def __init__(self, value: int):
+        self.value = value
+
+class CharLiteralNode(ParseTreeNode):
+    def __init__(self, value: str):
+        self.value = value
+
+class ExpressionNode(ParseTreeNode):
+    def __init__(self, char: CharLiteralNode, string: StringLiteralNode, \
+        udi: UserDefinedNode, num: NumLiteralNode):
+        if char:
+            self.type = 'char'
+            self.value = char
+        elif string:
+            self.type = 'str'
+            self.value = string
+        elif num:
+            self.type = 'num'
+            self.value = num
+        elif udi:
+            self.type = 'userdefined'
+            self.value = udi
+        else:
+            raise Exception('nothing matches on expr node')
         
 class FixDeclarationNode(ParseTreeNode):
     def __init__(self, typespec: TypeNode, identifier: UserDefinedNode, exp: ExpressionNode, \
@@ -60,44 +106,28 @@ class DeclarationNode(ParseTreeNode):
             raise Exception("nothing matches in declaration node")  # shouldn't happen ofc
 
 class FunctionNode(ParseTreeNode):
-    def __init__(self, name: UserDefinedNode, arg: list, return_type: TypeNode, \
+    def __init__(self, name: UserDefinedNode, args: list, return_type: TypeNode, \
          declarations: list, statements: list):
-        # args is most likely a list of parameter objects
-        # args may be empty
-        # return_type may be None
-        # decs & statements are lists of their respective nodes, potentially empty
-        pass
-
-class TypeNode(ParseTreeNode):
-    def __init__(self, value: str):
-        self.value = value  # num, ascii
-
-class StatementNode(ParseTreeNode):
-    def __init__(self, a_node: AssignmentNode, r_node: ReturnNode, s_node: SelectionNode,\
-         l_node: LoopNode, f_node: CallNode):
-        # indicate the type of the statement and its associated info
-        if a_node:
-            self.type = 'assign'
-            self.value = a_node
-        elif r_node:
-            self.type = 'return'
-            self.value = r_node
-        elif s_node:
-            self.type = 'selection'
-            self.value = s_node
-        elif l_node:
-            self.type = 'loop'
-            self.value = l_node
-        elif f_node:
-            self.type = 'funcall'
-            self.value = f_node
-        else:
-            raise Exception("nothing matches on statement node")
-
-class CallNode(ParseTreeNode):  # function call
-    def __init__(self, name: IdentifierNode, args: list):
         self.name = name
-        self.args = args  # list[ExpressionNode]
+        self.args = args
+        self.return_type = return_type  # can be None
+        self.declarations = declarations
+        self.statements = statements
+
+    def display(self):
+        print('--func.name--')
+        self.name.display()
+        print('--func.args--')
+        [arg.display() for arg in self.args]
+        print('--func.return_type--')
+        if self.return_type:
+            self.return_type.display()
+        else:
+            print('None')
+        print('--func.decs--')
+        [dec.display() for dec in self.declarations]
+        print('--func.stats--')
+        [stat.display() for stat in self.statements]
 
 class AssignmentNode(ParseTreeNode):
     def __init__(self, identifier: UserDefinedNode, exp: ExpressionNode, \
@@ -115,43 +145,6 @@ class AssignmentNode(ParseTreeNode):
         else:
             raise Exception("no match on assign node")
 
-class IdentifierNode(ParseTreeNode):
-    def __init__(self, udi: UserDefinedNode, res: ReservedNode):
-        if udi:
-            self.type = 'userdefined'
-            self.value = udi
-        elif res:
-            self.type = 'reserved'
-            self.value = res
-        else:
-            raise Exception("nothing matches in id node")
-
-class UserDefinedNode(ParseTreeNode):
-    def __init__(self, name: str):
-        self.name = name
-
-class ReservedNode(ParseTreeNode):
-    def __init__(self, value: str):
-        self.value = value  # write or read
-
-# i think these will be needed for static semantics
-class StringLiteralNode(ParseTreeNode):
-    def __init__(self, value: str):
-        self.value = value
-
-class NumLiteralNode(ParseTreeNode):
-    def __init__(self, value: int):
-        self.value = value
-        # i wonder if we'll need a sign property (positive, negative) for the AL
-
-class CharLiteralNode(ParseTreeNode):
-    def __init__(self, value: str):
-        self.value = value
-
-class ExpressionNode(ParseTreeNode):
-    def __init__(self, type, value):  # uhmm
-        pass
-
 class ParamNode(ParseTreeNode):
     def __init__(self, type: TypeNode, name: UserDefinedNode):
         self.type = type
@@ -167,3 +160,67 @@ class ReturnNode(ParseTreeNode):
             self.value = call
         else:
             raise Exception('nothing matches on return node')
+
+class SelectionNode(ParseTreeNode):
+    def __init__(self):
+        pass
+
+class LoopNode(ParseTreeNode):
+    def __init__(self):
+        pass
+
+class StatementNode(ParseTreeNode):
+    def __init__(self, a_node: AssignmentNode, r_node: ReturnNode, s_node: SelectionNode,\
+         l_node: LoopNode, f_node: CallNode):
+        if a_node:
+            self.type = 'assign'
+            self.value = a_node
+        elif r_node:
+            self.type = 'return'
+            self.value = r_node
+        elif s_node:
+            self.type = 'selection'
+            self.value = s_node
+        elif l_node:
+            self.type = 'loop'
+            self.value = l_node
+        elif f_node:
+            self.type = 'funcall'
+            self.value = f_node
+        else:
+            raise Exception("nothing matches on statement node")
+    
+    def display(self):
+        print('--stat.type--')
+        print(self.type)
+        print(f'--stat.{self.type}')
+        self.value.display()            
+
+class MainNode(ParseTreeNode):  # this means inheritance
+    def __init__(self, declarations: list, statements: list):
+        # we won't implement nested functions
+        self.declarations = declarations  # of type DeclarationNode
+        self.statements = statements  # of type StatementNode
+
+    def display(self):
+        print('--main.decs--')
+        [dec.display() for dec in self.declarations]
+        print('--main.stats--')
+        [stat.display() for stat in self.statements]
+    
+class ProgramNode(ParseTreeNode):
+    def __init__(self, declarations: list, functions: list, main_node: MainNode):
+        if not main_node:
+            raise Exception("Input program lacks entry function.")
+        self.declarations = declarations
+        self.functions = functions
+        self.main_node = main_node
+    
+    def display(self):
+        print('--prog.main--')
+        self.main_node.display()
+        print('--prog.decs--')
+        [dec.display() for dec in self.declarations]
+        print('--prog.funcs--')
+        [func.display() for func in self.functions]
+        
